65323ff8744fa509c8c5e2e26b38ca385909ea29
Matthew Horridge <matthew.horridge@stanford.edu>
2017-06-12 15:02:01 -0700

    Added a PortletModule annotation for auto-inclusion of portlet modules

---
+++ src/main/java/edu/stanford/webprotege/shared/annotations/PortletModule.java
@@ -0,0 +1,16 @@
+package edu.stanford.webprotege.shared.annotations;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+/**
+ * Matthew Horridge
+ * Stanford Center for Biomedical Informatics Research
+ * 12 Jun 2017
+ *
+ * Represents a module for specifying dependencies for portlets.
+ */
+@Retention(RetentionPolicy.RUNTIME)
+public @interface PortletModule {
+
+}
